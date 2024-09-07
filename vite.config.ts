d5aaafa36b7c946c781fa8ddcb43522b7be16e52
Satwinder04 <satwindersalan@gmail.com>
2024-09-07 23:51:15 +0530

    chunk

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    chunkSizeWarningLimit: 1000,  // Set limit to 1000 KB (or any desired size)
+  },
 })
